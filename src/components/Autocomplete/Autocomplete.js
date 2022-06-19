c1ce4713751eb4467e08f79d31b20382bbf5eb20
Francisco de Sautu Riestra <fdesautu@gmail.com>
2022-06-19 08:40:56 -0300

    Added some events for searchbox

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./autocomplete.module.scss";
 
 const AutoComplete = ({ suggestions }) => {
+  const [searchBoxValue, setSearchBoxValue] = useState("");
+  const searchBoxRef = useRef();
+
+  const handleInputOnChange = (e) => {
+    setSearchBoxValue(e.target.value);
+    console.log(searchBoxValue);
+  }  
+
+   const handleInputOnKeyDown = (e) => {
+     console.log(e.keyCode);
+     if (e.keyCode === (27)  || e.keyCode === (13)) setSearchBoxValue("");
+   };  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e);
   };
+
+  useEffect(() => {
+    searchBoxRef.current.focus();
+  }, [])
+  
   return (
     <>
       <form className={styles.autocompleteWrapper} onSubmit={handleSubmit}>
@@ -13,6 +31,10 @@ const AutoComplete = ({ suggestions }) => {
           type="text"
           placeholder="Nunca dejes de buscar"
           className={styles.navInputSearch}
+          value={searchBoxValue}
+          ref={searchBoxRef}
+          onChange={handleInputOnChange}
+          onKeyDown={handleInputOnKeyDown}
         />
         <button type="submit" className={styles.searchButton}>
           <div
